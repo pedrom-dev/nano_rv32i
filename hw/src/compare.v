35284db831912f5dab0f0d95c0d2f0b6361c12a8
pedrom-dev <020400pm@gmail.com>
2024-12-01 13:19:58 +0100

    Fix compare module logic for branch operations (BLT, BGE, etc.)

--- hw/src/compare.v
+++ hw/src/compare.v
@@ -2,7 +2,7 @@ module compare (
     input wire branch_i,
     input wire zero_i,
     input wire alu_result_i,
-    input wire [3:0] funct3_i,
+    input wire [2:0] funct3_i,
     
     output reg take_branch_o
     
@@ -14,10 +14,10 @@ module compare (
             case (funct3_i)
                 4'b0000: take_branch_o = zero_i;            // BEQ
                 4'b0001: take_branch_o = ~zero_i;           // BNE
-                4'b0100: take_branch_o = alu_result_i;      // BLT
-                4'b0101: take_branch_o = ~alu_result_i;     // BGE
+                4'b1001: take_branch_o = alu_result_i;      // BLT
+                4'b1001: take_branch_o = alu_result_i;     // BGE
                 4'b0110: take_branch_o = alu_result_i;      // BLTU
-                4'b0111: take_branch_o = ~alu_result_i;     // BGEU
+                4'b0111: take_branch_o = alu_result_i;     // BGEU
                 default: take_branch_o = 1'b0;
             endcase
         end
