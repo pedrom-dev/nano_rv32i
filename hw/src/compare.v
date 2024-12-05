2cb636bfda46b6d742bbc82119795f7f9c1329cf
pedrom-dev <020400pm@gmail.com>
2024-12-05 11:49:44 +0100

    Fix compare: remove alu_result_i

--- hw/src/compare.v
+++ hw/src/compare.v
@@ -1,9 +1,7 @@
 module compare (
     input wire branch_i,
     input wire zero_i,
-    input wire [31:0] alu_result_i,
     input wire [2:0] funct3_i,
-    
     output reg take_branch_o
     
 );
@@ -12,12 +10,12 @@ module compare (
         take_branch_o = 1'b0;
         if (branch_i) begin
             case (funct3_i)
-                3'b000: take_branch_o = zero_i;            // BEQ
+                3'b000: take_branch_o = zero_i;           // BEQ
                 3'b001: take_branch_o = ~zero_i;           // BNE
-                3'b100: take_branch_o = alu_result_i;      // BLT
-                3'b101: take_branch_o = alu_result_i;      // BGE
-                3'b110: take_branch_o = alu_result_i;      // BLTU
-                3'b111: take_branch_o = alu_result_i;      // BGEU
+                3'b100: take_branch_o = ~zero_i;      // BLT
+                3'b101: take_branch_o = ~zero_i;      // BGE
+                3'b110: take_branch_o = ~zero_i;      // BLTU
+                3'b111: take_branch_o = ~zero_i;      // BGEU
                 default: take_branch_o = 1'b0;
             endcase
         end
